17bc13db0c5a4c5024ac1c76a309522375572721
Yan Tsenter <yantsenter@ITs-MacBook-Pro.lawrenceville.org>
2021-03-30 19:03:09 -0400

    statics

--- chatsite/settings.py
+++ chatsite/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # mysite/settings.py
 # Channels
